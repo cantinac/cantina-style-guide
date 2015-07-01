a8d2fd62d3d9ea3ce303ae435748157ebd66a6c8
Conor Sheehan <conor@cantinaconsulting.com>
2015-06-30 20:37:14 -0400

    Add code previews to styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -37,6 +37,55 @@ var sectionTitleController = {
             sectionTitleScene.destroy();
         }
     }
+};
+
+var styleController = {
+    initialize: function() {
+        this.$styles = $('.style');
+        this.updateAllStyles();
+        return this;
+    },
+
+    updateAllStyles: function() {
+        var self = this;
+        this.$styles.each(function() {
+            var $style = $(this);
+            self.addCodePreviewToStyle($style);
+        });
+        return this;
+    },
+
+    addCodePreviewToStyle: function($style) {
+        var $codePreview = this.getCodePreviewForStyle($style);
+        $style.find('.style-description').after($codePreview);
+        return this;
+    },
+
+    getCodePreviewForStyle: function($style) {
+        var htmlSrc = this.getHTMLforStyle($style);
+        var $codePreview = $(this.getEmptyCodePreview());
+        var $code = $codePreview.find('code');
+        $code.text(htmlSrc);
+        return $codePreview;
+    },
+
+    getHTMLforStyle: function($style) {
+        var html = $style.find('.style-preview')[0].innerHTML;
+        var htmlLines = _.filter(html.split('\n'), function(line) {
+            return line && $.trim(line);
+        });
+        var spaces = _.sortBy(htmlLines, function(line) {
+            return line.search(/\S/);
+        })[0].search(/\S/);
+        var newHtmlLines = _.map(htmlLines, function(line) {
+            return line.slice(spaces);
+        });
+        return newHtmlLines.join('\n');
+    },
+
+    getEmptyCodePreview: function() {
+        return '<div class="style-code"><pre><code class="language-markup" id="test"></code></pre></div>';
+    }
 }
 
 
@@ -45,4 +94,5 @@ $(function () {
     var scrollController = new ScrollMagic.Controller();
 
     sectionTitleController.initialize(scrollController);
+    styleController.initialize();
 });
