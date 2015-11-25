3c895968e05bce21fc528c722f88ef1b7b5d1183
Scott Doxey <neogeek0101@gmail.com>
2015-11-25 10:36:28 -0500

    Updated to latest style guide.

--- app/scripts/main.js
+++
@@ -1,9 +0,0 @@
-/* jshint devel:true */
-
-// $(function () {
-//     var scrollController = new ScrollMagic.Controller();
-
-//     styleController.initialize();
-//     colorSwatchController.initialize();
-//     sectionTitleController.initialize(scrollController);
-// });
